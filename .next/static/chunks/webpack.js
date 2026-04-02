96f7bbf547b74b304283488a0cb57a95529f429a
VictorSerradell <victorserradellpinto@hotmail.com>
2026-04-02 11:08:10 +0200

    fix: panel top:64 respects header, endurance duration badge, teamDriving badge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5625ccaa1d711dc7"; }
+/******/ 		__webpack_require__.h = function() { return "c02dcd856e95e7de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
